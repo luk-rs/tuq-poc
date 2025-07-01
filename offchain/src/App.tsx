8345fe29e342b69db285995a81328db2f0eb1429
luk <santos8@gmail.com>
2025-07-01 19:17:11 +0100

    initial structure for contracts (Aiken) and site (React + Blockfrost + Lucid)

---
+++ offchain/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1>Bem-vindo ao teu Cardano dApp!</h1>
+      <p>Configura as tuas carteiras (Nami, Eternl, Lace) e começa a codar.</p>
+    </div>
+  );
+}
+
+export default App;
