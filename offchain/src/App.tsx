0580cb6470fe48692f1dd7f78aa77de8729ac220
luk <santos8@gmail.com>
2025-07-02 16:41:32 +0000

    test update

--- offchain/src/App.tsx
+++ offchain/src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 function App() {
   return (
     <div className="p-4">
-      <h1>Bem-vindo ao teu Cardano dApp!</h1>
+      <h1>Bem-vindo ao teu Cardano dApp! Santos</h1>
       <p>Configura as tuas carteiras (Nami, Eternl, Lace) e começa a codar.</p>
     </div>
   );
