15f80f25e6082ed23da17f25c0a5cfd65b50efd9
luk <santos8@gmail.com>
2025-08-09 16:46:13 +0100

    setup tailwind correctly

--- offchain/src/App.tsx
+++ offchain/src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <div className="p-4">
-      <h1>Bem-vindo ao teu Cardano dApp! Santos</h1>
-      <p>Configura as tuas carteiras (Nami, Eternl, Lace) e começa a codar.</p>
+    <div className="p-4 mt-10 bg-green-500">
+      <h1 className="text-white">Bem-vindo ao teu Cardano dApp! Santos</h1>
+      <p className="text-blue-200">
+        Configura as tuas carteiras (Nami, Eternl, Lace) e começa a codar.
+      </p>
     </div>
   );
 }
